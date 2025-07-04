593685b9db191c306a750db2704fe514d39c5b30
VIGNESH KARIMIKONDA <vigneshkarimikonda@VIGNESHs-MacBook-Air.local>
2025-07-04 13:09:51 +0530

    done week-3 exe1

---
+++ week-3/Mandatory-Hands-On/Exe1BasicSpringApp/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,17 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    // Setter for Spring XML injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(String bookName) {
+        System.out.println("Adding book: " + bookName);
+        bookRepository.saveBook(bookName);
+    }
+}
